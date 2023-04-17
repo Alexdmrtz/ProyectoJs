c5f8d15265069c1119fcf8d84a7739b4f5f3e8a3
Alexis Martinez <AlexMartinez_156@hotmail.com>
2023-04-17 19:11:43 -0300

    volvi a modificar el codigo

--- main.js
+++ main.js
@@ -10,38 +10,47 @@ function simularPlazoFijo(capitalInicial, tasaAnual, plazoMeses, cantidadPeriodo
   if (isNaN(tasaAnual) || tasaAnual <= 0) {
     return "La tasa anual ingresada no es válida.";
   }
-  if (isNaN(plazoMeses) || plazoMeses <= 0) {
+  if (isNaN(plazoMeses) || plazoMeses < 1) {
     return "El plazo en meses ingresado no es válido.";
   }
-  if (isNaN(cantidadPeriodos) || cantidadPeriodos <= 0) {
-    return "La cantidad de períodos ingresada no es válida.";
-  }
+
 
   // Cálculo de la tasa mensual y del interés ganado
 
-  let tasaMensual = tasaAnual / 12;
-  let interesGanado = capitalInicial * tasaMensual * (plazoMeses / 12);
+  let tasaMensual = tasaAnual / 12 / 100;
+  let interesGanado = 0;
 
   // Cálculo del monto total al vencimiento para cada período y acumulación de los resultados
 
-  let montoTotalAcumulado = capitalInicial + interesGanado;
-  for (let i = 2; i <= cantidadPeriodos; i++) {
-    interesGanado = montoTotalAcumulado * tasaMensual * (plazoMeses / 12);
-    montoTotalAcumulado += interesGanado;
+  for (let i = 1; i <= plazoMeses; i++) {
+    let interesMensual = capitalInicial * tasaMensual;
+    capitalInicial += interesMensual;
+    interesGanado += interesMensual;
+    console.log(`Interés ganado en el mes ${i}: ${interesMensual.toFixed(2)}`);
   }
+
+  let montoTotal = capitalInicial;
+  console.log(`Monto total al vencimiento: ${montoTotal.toFixed(2)}`);
 
   // Devolución del resultado formateado
 
-  return `Al vencimiento de ${cantidadPeriodos} períodos de ${plazoMeses} meses, su plazo fijo de $${capitalInicial} a una tasa del ${tasaAnual}% anual habrá generado un interés total de $${(montoTotalAcumulado - capitalInicial).toFixed(2)}, y el monto total acumulado será de $${montoTotalAcumulado.toFixed(2)}.`;
+  return `Al vencimiento, su plazo fijo de $${capitalInicial.toFixed(2)} a una tasa del ${tasaAnual}% anual por ${plazoMeses} meses habrá generado un interés de $${interesGanado.toFixed(2)}, y el monto total será de $${montoTotal.toFixed(2)}.`;
 }
 
 // Ejemplo de uso
 
-let capital = prompt("Ingrese el capital a invertir:");
-let tasa = prompt("Ingrese la tasa anual en porcentaje:");
-let plazo = prompt("Ingrese el plazo en meses:");
-let cantidadPeriodos = prompt("Ingrese la cantidad de períodos:");
+let continuar = true;
 
-let resultado = simularPlazoFijo(parseFloat(capital), parseFloat(tasa), parseFloat(plazo), parseInt(cantidadPeriodos));
-alert(resultado);
+while (continuar) {
+  let capital = prompt("Ingrese el capital a invertir:");
+  let tasa = 78;
+  let plazo = prompt("Ingrese el plazo en meses:");
+
+  let resultado = simularPlazoFijo(parseFloat(capital), parseFloat(tasa), parseFloat(plazo));
+  alert(resultado);
+
+  if (!confirm("Desea seguir operando?")) {
+    continuar = false;
+  }
+}
 
